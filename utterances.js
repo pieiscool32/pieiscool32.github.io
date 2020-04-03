eac9f5e0a16f245bccbf9ced6d035e113d7cc0f2
Chris Munte <cmunte132@gmail.com>
2020-04-03 13:24:39 -0400

    Updated utterances location and gemfile

---
+++ utterances.js
@@ -0,0 +1,8 @@
+<script src="https://utteranc.es/client.js"
+    repo="pieiscool32/pieiscool32.github.io"
+    issue-term="pathname"
+    label="Blog-Comment"
+    theme="github-light"
+    crossorigin="anonymous"
+async>
+</script>

a92853c0c06d9955ead5bb84f887e833b6e4e42e
Chris Munte <cmunte132@gmail.com>
2020-04-03 14:39:16 -0400

    Directly imbed utterances

--- utterances.js
+++
@@ -1,8 +0,0 @@
-<script src="https://utteranc.es/client.js"
-    repo="pieiscool32/pieiscool32.github.io"
-    issue-term="pathname"
-    label="Blog-Comment"
-    theme="github-light"
-    crossorigin="anonymous"
-async>
-</script>
